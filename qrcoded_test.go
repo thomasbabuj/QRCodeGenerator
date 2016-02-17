ddd1744b41a7168a74b8d92e7e18c4ccb2b8a00f
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-02-17 15:59:14 +0800

    verifying that GenerateQRCode returns a useful value

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -1,2 +1,16 @@
 //QRCoded file test cases
 package main
+
+import "testing"
+
+func TestGenerateQRCodeReturnsValue(t *testing.T) {
+	result := GenerateQRCode("555-2368")
+
+	if result == nil {
+		t.Error("Generated QRCode is nil")
+	}
+
+	if len(result) == 0 {
+		t.Error("Generated QRCode has no data")
+	}
+}
