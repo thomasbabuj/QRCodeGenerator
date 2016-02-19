9ccba77b36191f7472d7609a8c4c13c8ea48bbe6
Thomas Babu <thomas@interuptive.com>
2016-02-19 17:03:07 +0800

    added decoding logic

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -1,7 +1,11 @@
 //QRCoded file test cases
 package main
 
-import "testing"
+import (
+	"bytes"
+	"image/png"
+	"testing"
+)
 
 func TestGenerateQRCodeReturnsValue(t *testing.T) {
 	result := GenerateQRCode("555-2368")
@@ -12,5 +16,18 @@ func TestGenerateQRCodeReturnsValue(t *testing.T) {
 
 	if len(result) == 0 {
 		t.Error("Generated QRCode has no data")
+	}
+}
+
+/**
+TestGenerateQRCodeGeneratesPNG contains decoding logic.
+*/
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
+	result := GenerateQRCode("554-2368")
+	buffer := bytes.NewBuffer(result)
+
+	_, err := png.Decode(buffer)
+	if err != nil {
+		t.Errorf("Generated QRCode is not a PNG : %s", err)
 	}
 }
